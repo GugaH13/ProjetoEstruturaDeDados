6e52cb0bc04ce1401bc6d89a415b7558ee74991e
GustavoHMeira <cc18015@g.unicamp.br>
2019-03-22 11:31:47 -0300

    a

--- 18015_18180_Projeto1ED/18015_18180_Projeto1ED/ListaLigadaCruzada.cs
+++ 18015_18180_Projeto1ED/18015_18180_Projeto1ED/ListaLigadaCruzada.cs
@@ -8,18 +8,33 @@ using System.Threading.Tasks;
 public class ListaLigadaCruzada
 {
     Celula noCabeca;
+    int numeroLinhas, numeroColunas;
 
     public Celula NoCabeca { get => noCabeca; set => noCabeca = value; }
 
-    public ListaLigadaCruzada(Celula noCabeca)
+    public ListaLigadaCruzada(Celula noCabeca, int nl, int nc)
     {
         this.noCabeca = noCabeca;
+        this.numeroLinhas = nl;
+        this.numeroColunas = nc;
+        Construir(nl,nc);
     }
 
-    public bool ExisteCelula(Celula c)
+    protected void Construir(int nl, int nc)
+    {
+        int nAtualL = 0, nAtualC = 0;
+
+        while (nAtualL < nl && nAtualC < nc)
+        {
+            
+        }
+    }
+
+
+   /* public bool ExisteCelula(Celula c)
     {
         Celula atual = noCabeca;
         
         while ()
-    }
+    }*/
 }
